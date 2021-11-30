c2da83de2f527002cd11b7849ce6f8eb69748a2d
huangshuai <huangshuai@ctfo.com>
2021-11-30 16:42:55 +0800

    lsp-lua: setup lsp for lua.

--- lua/hs-lspconfig.lua
+++ lua/hs-lspconfig.lua
@@ -55,9 +55,46 @@ local function setup_python()
 	}
 end
 
+local function setup_lua()
+	-- https://github.com/neovim/nvim-lspconfig/blob/master/CONFIG.md#sumneko_lua
+	local sumneko_root_path = vim.fn.stdpath('cache')..'/lua-language-server'
+	local sumneko_binary = sumneko_root_path.."/bin/Linux/lua-language-server"
+
+	local runtime_path = vim.split(package.path, ';')
+	table.insert(runtime_path, "lua/?.lua")
+	table.insert(runtime_path, "lua/?/init.lua")
+
+	require'lspconfig'.sumneko_lua.setup {
+		cmd = { sumneko_binary, "-E", sumneko_root_path .. "/main.lua" },
+		on_attach = on_attach,
+		capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+		flags = {
+			debounce_text_changes = 150,
+		},
+		settings = {
+			Lua = {
+				runtime = {
+					version = 'LuaJIT',
+					path = runtime_path,
+				},
+				diagnostics = {
+					globals = {'vim'},
+				},
+				workspace = {
+					library = vim.api.nvim_get_runtime_file("", true),
+				},
+				telemetry = {
+					enable = false,
+				}
+			}
+		}
+	}
+end
+
 function M.setup()
 	disable_diagnostic_msg()
 	setup_python()
+	setup_lua()
 end
 
 return M
