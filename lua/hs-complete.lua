87c9bfbdca706649598461506edd38514499a26e
huangshuai <huangshuai@ctfo.com>
2022-04-26 16:15:32 +0800

    completion: 更新插件后, 更新配置

--- lua/hs-complete.lua
+++ lua/hs-complete.lua
@@ -12,15 +12,22 @@ function M.setup()
 		mapping = {
 		  ['<C-d>'] = cmp.mapping.scroll_docs(-4),
 		  ['<C-f>'] = cmp.mapping.scroll_docs(4),
+		  ['<C-n>'] = cmp.mapping.select_next_item(),
+		  ['<C-p>'] = cmp.mapping.select_prev_item(),
 		  ['<C-e>'] = cmp.mapping.close(),
 		  ['<CR>'] = cmp.mapping.confirm({ select = true })
 		},
-		sources = {
-		  { name = 'nvim_lsp' },
-		  { name = 'ultisnips' },
-		  { name = 'buffer' }
-		}
+        sources = cmp.config.sources({
+            { name = 'nvim_lsp' },
+            { name = 'ultisnips' },
+        }, {
+            { name = 'buffer' },
+        })
 	})
+
+    local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+    cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
+    cmp_autopairs.lisp[#cmp_autopairs.lisp+1] = "racket"
 end
 
 return M
