2c1397b867840d94c3e99457e27a8af38be741b5
huangshuai <huangshuai@ctfo.com>
2023-01-11 21:16:33 +0800

    telescope: change keybindings

--- lua/hs-telescope.lua
+++ lua/hs-telescope.lua
@@ -16,15 +16,14 @@ M.setup = function ()
 
   pcall(require('telescope').load_extension, 'fzf')
 
-  vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-  vim.keymap.set('n', '<leader>sb', function() require("telescope.builtin").buffers({only_cwd=true, sort_mru=true}) end)
-  vim.keymap.set('n', '<leader>sB', function() require("telescope.builtin").buffers({sort_mru=true}) end)
-  vim.keymap.set('n', '<leader>/', function() require('telescope.builtin').current_buffer_fuzzy_find() end, { desc = '[/] Fuzzily search in current buffer]' })
+  vim.keymap.set('n', '<leader>h', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
+  vim.keymap.set('n', '<leader>b', function() require("telescope.builtin").buffers({only_cwd=true, sort_mru=true}) end)
+  vim.keymap.set('n', '<leader>B', function() require("telescope.builtin").buffers({sort_mru=true}) end)
+  -- vim.keymap.set('n', '<leader>/', function() require('telescope.builtin').current_buffer_fuzzy_find() end, { desc = '[/] Fuzzily search in current buffer]' })
 
-  vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+  vim.keymap.set('n', '<leader>p', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
   vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
-  vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
-  vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+  vim.keymap.set('n', '<leader>ag', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 end
 
 return M
