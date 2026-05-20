79c2de76c7e60dd8e6962d7706d0739052a72564
huangshuai <huangshuai_010@163.com>
2026-05-20 20:36:16 +0800

    feat(plugins): 添加 vim-dadbod-ui 及 SQL 补全集成
    新增 vim-dadbod-ui 及其依赖 vim-dadbod、vim-dadbod-completion，
    将 dadbod 注册为 blink.cmp 数据源，使 SQL buffer 支持表名/列名
    补全。提供 <leader>db 在新 tab 中打开 DBUI。

---
+++ lua/plugins/db.lua
@@ -0,0 +1,40 @@
+return {
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = {
+      { "tpope/vim-dadbod", lazy = true },
+      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+    },
+    keys = {
+      { "<cr>", "vap|<Plug>(DBUI_ExecuteQuery)", ft = "mysql" },
+      { "<cr>", "<Plug>(DBUI_ExecuteQuery)", mode = "v", ft = "mysql" },
+      { "<leader>db", "<cmd>TabooOpen DB<cr><cmd>DBUI<cr>", desc = "Open DBUI in new tab" },
+    },
+    cmd = {
+      "DBUI",
+      "DBUIToggle",
+      "DBUIAddConnection",
+      "DBUIFindBuffer",
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
+  },
+  {
+    "saghen/blink.cmp",
+    dependencies = { "kristijanhusak/vim-dadbod-completion" },
+    opts = function(_, opts)
+      opts.sources = opts.sources or {}
+      opts.sources.default = opts.sources.default or {}
+      if not vim.tbl_contains(opts.sources.default, "dadbod") then
+        table.insert(opts.sources.default, "dadbod")
+      end
+      opts.sources.providers = opts.sources.providers or {}
+      opts.sources.providers.dadbod = {
+        name = "Dadbod",
+        module = "vim_dadbod_completion.blink",
+      }
+    end,
+  },
+}
