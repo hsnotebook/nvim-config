a141d29f6982e9efbfd62e26d2ae63dccc71880e
huangshuai <huangshuai_010@163.com>
2026-05-20 19:53:57 +0800

    feat(plugins): 添加 snacks picker C-v 垂直分屏快捷键

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -7,12 +7,14 @@ return {
           input = {
             keys = {
               ["<C-x>"] = { "edit_split", mode = { "i", "n" } },
+              ["<C-v>"] = { "edit_vsplit", mode = { "i", "n" } },
               ["<C-s>"] = false,
             },
           },
           list = {
             keys = {
               ["<C-x>"] = "edit_split",
+              ["<C-v>"] = "edit_vsplit",
               ["<C-s>"] = false,
             },
           },
