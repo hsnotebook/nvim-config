fe2ea1f411412489ba22b3e31d3657b6ad38f176
huangshuai <huangshuai_010@163.com>
2026-05-22 10:41:10 +0800

    fix(plugins): 禁用保存 SQL 文件时自动执行查询
    设置 db_ui_execute_on_save = 0，避免保存时意外触发 SQL 执行。

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -8,6 +8,34 @@ local function augroup(name)
   return vim.api.nvim_create_augroup("hs_" .. name, { clear = true })
 end
 
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("java"),
+  pattern = { "java" },
+  callback = function()
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("markdown"),
+  pattern = { "markdown" },
+  callback = function()
+    vim.opt_local.tabstop = 4
+    vim.opt_local.shiftwidth = 4
+  end,
+})
+
+-- `vim.opt.spell = false` in options.lua does not work.
+-- So use autocmds to config `spell`
+vim.api.nvim_create_autocmd("FileType", {
+  group = augroup("spell"),
+  pattern = { "markdown", "gitcommit" },
+  callback = function()
+    vim.opt_local.spell = false
+  end,
+})
+
 -- SSH 环境下发送输入法切换信号
 local function send_input_method_signal(event)
   local ssh_client_ip = os.getenv("SSH_CONNECTION"):gmatch("%S+")()
